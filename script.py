6ca1a8bb69147b62843f8fab4a4c0c285d427d3f
g.sawada <sawada.gota@gmail.com>
2024-05-27 22:56:52 +0900

    jupyter notebookの導入とxlsxファイル読み込みのテスト

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-df = pd.DataFrame({
-      '名前': ["佐藤", "斎藤", "鈴木"],
-      '年齢': [24, 45, 36]
-    })
-print(df)
+# df = pd.DataFrame({
+#       '名前': ["佐藤", "斎藤", "鈴木"],
+#       '年齢': [24, 45, 36]
+#     })
+# print(df)
 
-df pd.read_xlsx('list_of_observatories_240527DL.xlsx', sheet_name=3)
+# df = pd.read_excel('./raw/list_of_observatories_240527DL.xlsx', sheet_name=3)
+df = pd.read_excel('./raw/list_of_observatories_240527DL.xlsx', sheet_name=3)
+
+print(df)
