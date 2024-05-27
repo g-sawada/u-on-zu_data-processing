f97c7e85fafcccfbec3a1d40306d56cac7970600
g.sawada <sawada.gota@gmail.com>
2024-05-27 22:35:07 +0900

    観測所ローデータを配置

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
-print("Hello, World!")
-print(1+2)
+import pandas as pd
+
+df = pd.DataFrame({
+      '名前': ["佐藤", "斎藤", "鈴木"],
+      '年齢': [24, 45, 36]
+    })
+print(df)
+
+df pd.read_xlsx('list_of_observatories_240527DL.xlsx', sheet_name=3)
